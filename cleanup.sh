36f135561aca47259d6c76496214d1d7e22690b5
Jamie Duncan <jamieduncan@google.com>
2021-05-14 13:43:32 +0000

    refactoring cleanup.sh
    b/188049192
    
    Change-Id: I29658347e68cc3b834d5d434a49986003062f5b7

--- cleanup.sh
+++ cleanup.sh
@@ -2,17 +2,21 @@
 # Google Confidential, Pre-GA Offering for Google Cloud Platform 
 # (see https://cloud.google.com/terms/service-terms)
 
-export REGION="us-central1"
-export PROJECT_ID=$(gcloud config get-value core/project)
+REGION=$(gcloud config get-value compute/region)
+PROJECT_ID=$(gcloud config get-value core/project)
+BACKEND=$PROJECT_ID-tf-backend
 
-cd tf
-terraform destroy -auto-approve -var=project_id=${PROJECT_ID} -var=region=${REGION}
-cd ..
+ROOT_DIR=$(git rev-parse --show-toplevel)
+TF_DIR=$ROOT_DIR/tf
+CD_CONFIG_DIR=$ROOT_DIR/clouddeploy-config
 
-rm -rf ./web
-rm -rf ./web-helm
-rm -rf tf/.terraform
-rm -rf tf/main.tf
-rm -rf tf/terraform.tfstat*
-rm -rf tf/terraform.tfplan
-rm -rf confi* && mkdir config
+cd $TF_DIR
+terraform destroy -auto-approve -var=project_id=$PROJECT_ID -var=region=$REGION
+cd $ROOT_DIR
+
+gsutil rm -r gs://$BACKEND/
+
+rm -rf $TF_DIR/.terraform
+rm -rf $TF_DIR/main.tf
+rm -rf $TF_DIR/terraform.tfstat*
+rm -rf $TF_DIR/terraform.tfplan

3e21dff4881360620548a08afc6af5b626f20f0c
henrybell <henrybell@google.com>
2023-04-06 12:22:12 +0100

    Add warm-up for traffic to new revisions
    Change-Id: Ie5f491765aa0a2ff037b3dae7ec495a14111aed4

--- tutorials/deployment-strategies-run/scripts/call-cloudrun-service.sh
+++ tutorials/deployment-strategies-run/scripts/call-cloudrun-service.sh
@@ -17,8 +17,16 @@ gcurl() {
   curl -H "Authorization: Bearer ${1}" "${2}"
 }
 
+gcurl-quiet() {
+  curl -S -s -o /dev/null -H "Authorization: Bearer ${1}" "${2}"
+}
+
+warm-gcurl() {
+  for i in {1..20} ; do gcurl-quiet ${1} ${2}; done
+}
+
 multi-gcurl() {
-  for i in {1..10} ; do printf "%2d. " ${i} && gcurl ${1} ${2}; done
+  for i in {1..10} ; do printf "%2d. " ${i} && gcurl ${1} ${2}; sleep 1; done
 }
 
 if [ "$#" -ne 1 ]; then
@@ -30,4 +38,6 @@ SVC=${1}
 TOKEN=$(gcloud auth print-identity-token)
 URL=$(gcloud run services describe demo-app-${SVC} --format='value(status.url)')
 
+warm-gcurl ${TOKEN} ${URL}
+sleep 1
 multi-gcurl ${TOKEN} ${URL}
